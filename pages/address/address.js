ee1dfa888a1281beb04626f5ce8c05b7cdf6aecb
wanggang <wanggang@mtscrm.com>
2018-04-03 08:01:09 +0800

    地址列表的处理

--- pages/address/address.js
+++ pages/address/address.js
@@ -1,66 +1,86 @@
-// pages/address/address.js
+let util = require('../../utils/util')
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    addressList: [{
+      recipients: '收货人',
+      recipientsPhone: "13478659803",
+      recipientsAddress: "收货人的地址",
+      isDefaultAddress: false
+    }]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
+  getAddressList: function () {
+    let _this = this;
+    return false;
+    util.ajax({
+      method: 'GET',
+      url: 'address',
+      success: function (res) {
+        let data = res.data.list;
+        _this.setData({
+          addressList: data
+        });
+      }
+    });
+  },
+  setDefaultAddress: function (e){
+    let _this = this;
+    let index = e.currentTarget.dataset.index;
+    let tmpArr = _this.data.addressList;
+    let cur = tmpArr[index];
+    cur.isDefaultAddress = !cur.isDefaultAddress;
+    _this.setData({
+      addressList: tmpArr
+    })
+  },
+  deleteConfirm: function (e){
+    let _this = this
+   
+    wx.showModal({
+      title: '提示',
+      content: '确定是否删除该地址',
+      success: function (res) {
+        if (res.confirm) {
+          let index = e.currentTarget.dataset.index
+          let address = _this.data.addressList[index]
+          console.log(address);
+          return false
+          let id = address.id
+          util.ajax({
+            method: 'PUT',
+            url: 'v1/orders/' + _this.data.orderId,
+            success: function (res) {
+              util.message({ content: '操作成功' })
+              wx.navigateBack({ delta: 1 })
+            }
+          });
+        }
+      }
+    })
+  },
   onLoad: function (options) {
-  
+    let _this = this;
+    _this.getAddressList();
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
+  },
   onShow: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
+  },
   onHide: function () {
-  
-  },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
+  },
   onUnload: function () {
-  
-  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
+  },
   onPullDownRefresh: function () {
-  
-  },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
+  },
   onReachBottom: function () {
-  
-  },
 
-  /**
-   * 用户点击右上角分享
-   */
+  },
   onShareAppMessage: function () {
-  
+
   }
 })
